a594df9b0420eb5ca3c7bbc0c909acc143314360
Joel Santana <joelsantzdev@gmail.com>
2019-07-23 02:16:44 -0500

    upload files project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Question from './components/Question';
+
 
 function App() {
+
+  // state
+  const [ budget, setBudget ] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <header>
+        <h1>Weekly Expense</h1>
+
+        <div className = "contenido-principal contenido">
+          <Question 
+            setBudget = {setBudget}
+          />
+        </div>
+      </header>  
     </div>
   );
 }
---
+++ src/components/Question.js
@@ -0,0 +1,54 @@
+import React, { Fragment, useState } from 'react';
+
+function Question(props) {
+    
+    const { setBudget } = props;
+
+    // Definir el state
+    const [ amount, setAmount] =  useState(0);
+    const [ error, setError ] = useState(false);
+
+    // Validar presupuesto
+    const addBudget = e => {
+        e.preventDefault();
+
+        
+
+        // Validar
+        if( amount < 1 || isNaN( amount ) ) {
+            setError(true);
+            return;
+        }
+
+        //  Si se pasa la validacion
+        setError(false);
+        setBudget( amount);
+    }
+
+
+    return (
+        <Fragment>
+            <h2>Put your Budget</h2>
+
+            {error ? <p className = "alert alert-danger error">Budget is Incorrect</p> : null}
+            <form
+                onSubmit = {addBudget}
+            >
+                <input type= "number" 
+                className = "u-full-width" 
+                placeholder = "add your budget"
+                onChange = {e => setAmount(parseInt(e.target.value, 10) ) }
+                />
+                <input type = "submit" 
+                className = "button-primary u-full-width"
+                value = "Define Budget"
+                />
+
+            </form>
+
+        </Fragment>
+    );
+
+}
+
+export default Question;
